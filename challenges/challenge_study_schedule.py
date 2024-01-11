4fd14f4cb02392a3091f87163e651bd1a7f1f9c1
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-01-11 18:38:02 -0300

    Feat: Requisito 01 finalizado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    try:
+        for key, value in permanence_period:
+            if key <= target_time <= value:
+                counter = counter + 1
+
+        return counter
+    except (TypeError):
+        return None
