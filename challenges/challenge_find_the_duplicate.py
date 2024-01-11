b593e1d08cc0ac1a64932752b49f3f39bff71332
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-01-11 20:21:02 -0300

    Feat: Requisito 05 finalizado

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,15 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (
+        not isinstance(nums, list)
+        or not all(isinstance(num, int) and num > 0 for num in nums)
+    ):
+        return False
+
+    seen_numbers = set()
+
+    for num in nums:
+        if num in seen_numbers:
+            return num
+        seen_numbers.add(num)
+
+    return False
