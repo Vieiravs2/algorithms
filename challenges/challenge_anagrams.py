ccaf45ebc27217534fc1938ec324654dc651dc43
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-01-11 19:49:27 -0300

    Feat: Requisito 04 finalizado

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,20 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+
+    if not first_string and not second_string:
+        return first_string, second_string, False
+
+    return (
+        order_string(first_string),
+        order_string(second_string),
+        order_string(first_string) == order_string(second_string)
+    )
+
+
+def order_string(string):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    return (
+        ''.join(letter * string.count(letter)
+                for letter in alphabet if letter in string)
+    )
