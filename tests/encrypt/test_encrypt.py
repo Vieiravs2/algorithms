39d6c1f0ba53d81896f1a7f4645ebedb5aaac16a
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-01-11 18:48:58 -0300

    Feat: Requisito 02 finalizado

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,10 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("Teste", 100) == "etseT"
+    assert encrypt_message("Testando", 3) == "seT_odnat"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("Testando a key com uma String", "A")
